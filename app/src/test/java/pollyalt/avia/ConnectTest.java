53f0e1fa7390a5cb3bf6bbf693f8009c6c047c80
altanab <badmalta@gmail.com>
2017-12-17 17:08:06 +0300

    added test

--- app/src/test/java/pollyalt/avia/ConnectTest.java
+++ app/src/test/java/pollyalt/avia/ConnectTest.java
@@ -9,13 +9,21 @@ import static org.junit.Assert.*;
  */
 
 public class ConnectTest {
+    private Connect testConnect = new Connect();
+
     @Test
-    public void testConnectClass() throws Exception {
-        Connect testConnect = new Connect();
+    public void testWrongRequest() throws Exception {
         String request = "";
         String answer = "\"Wrong request\"";
         assertEquals(testConnect.ConnectToServer(request), answer) ;
-        request = "/?from=Paris&where=Rome&when=2018-01-01";
+
+    }
+
+    @Test
+    public void testRightRequest() throws Exception{
+        String request = "/?from=Paris&where=Rome&when=2018-01-01";
+        String answer = "\"Wrong request\"";
         assertNotEquals(testConnect.ConnectToServer(request), answer);
     }
+
 }
